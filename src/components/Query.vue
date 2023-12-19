d25332e20e63f35653e215971b5aac6acb4797f6
louzeyu <709971694@qq.com>
2023-12-19 14:32:33 +0800

    打包配置 ，相关文件位置调整
    打包配置 ，相关文件位置调整

--- src/components/Query.vue
+++ src/components/Query.vue
@@ -73,7 +73,10 @@ const getCsvData = () => {
     let result = [];
     let headers = lines[0].split(",");
     for (let i = 1; i < lines.length; i++) {
-      let obj = {};
+      interface ob{
+        [key:string]:any
+      }
+      let obj:ob = {};
       let currentline = lines[i].split(",");
       for (let j = 0; j < headers.length; j++) {
         obj[headers[j]] = currentline[j];
